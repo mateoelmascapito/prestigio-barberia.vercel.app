e6058897cb1c32a55217f633e23d88dc66f245a7
Mateo Aparicio <mate2005apa@gmail.com>
2026-04-16 04:58:32 -0300

    Forzar actualizacion de package.json

---
+++ src/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BarberiaApp from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BarberiaApp />
+  </React.StrictMode>
+);
